02939d1e25524a7f050c95e2b2acfe1d535d19d0
afengbo <zfengfeng2@163.com>
2020-03-01 22:24:35 +0800

    会员列表

--- www.py
+++ www.py
@@ -13,6 +13,7 @@ from web.controllers.user.User import route_user
 from web.controllers.account.Account import route_account
 from web.controllers.finance.Finance import route_finance
 from web.controllers.food.Food import route_food
+from web.controllers.member.Member import route_member
 from web.controllers.stat.Stat import route_stat
 from web.controllers.static import route_static
 from web.controllers.api import route_api
@@ -22,6 +23,7 @@ app.register_blueprint(route_user, url_prefix="/user")
 app.register_blueprint(route_account, url_prefix="/account")
 app.register_blueprint(route_finance, url_prefix="/finance")
 app.register_blueprint(route_food, url_prefix="/food")
+app.register_blueprint(route_member, url_prefix="/member")
 app.register_blueprint(route_stat, url_prefix="/stat")
 app.register_blueprint(route_static, url_prefix="/static")
 app.register_blueprint(route_api, url_prefix="/api")
