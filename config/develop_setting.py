68adf2fb3177ec78bd3926ad3e0af4e5acdc2638
afengbo <zfengfeng2@163.com>
2020-03-07 08:30:51 +0800

    部署

--- config/develop_setting.py
+++ config/develop_setting.py
@@ -2,4 +2,14 @@
 # -*- coding: utf-8 -*-
 # Date: '2020/02/24 10:33'
 
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_DATABASE_URI = "mysql://root:333333@127.0.0.1/food_db?charset=utf8mb4"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENCODING = 'utf8mb4'
 
+APP = {
+    "domain": "https://food.fone.cn"
+}
+
+RELEASE_VERSION = '20200307001'
